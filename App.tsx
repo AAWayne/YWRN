92bc85ad3c183b742f4e50b679981bce499c9f0d
Wayne <Wayne.Yuangm@gmail.com>
2024-12-17 00:16:44 +0800

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,146 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import FastImage, {
+  ResizeMode,
+  OnLoadEvent,
+  OnProgressEvent,
+} from "react-native-fast-image";
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <FastImage
+              style={{
+                width: '100%',
+                height: 200,
+                margin: 20,
+              }}
+              source={{
+                uri: "https://res8.vmallres.com/pimages/uomcdn/CN/pms/202205/gbom/6941487259298/428_428_D7BFF22D4678EB68440F914B352214C4mp_tds.png",
+              }}
+              resizeMode={FastImage.resizeMode.contain}
+              onLoadStart={() => {
+                console.log("onLoadStart: success");
+              }}
+              onProgress={(e: OnProgressEvent) => {
+                console.log(
+                    "onProgress: success loaded=" +
+                    e.nativeEvent.loaded +
+                    " total=" +
+                    e.nativeEvent.total
+                );
+              }}
+              onLoad={(e: OnLoadEvent) => {
+                console.log(
+                    "onLoad: success width=" +
+                    e.nativeEvent.width +
+                    " height=" +
+                    e.nativeEvent.height
+                );
+              }}
+              onError={() => {
+                console.log("onError: success");
+              }}
+              onLoadEnd={() => {
+                console.log("onLoadEnd: success");
+              }}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,2 @@
+# YWRN
+ 鸿蒙版RN基础工程（compatibleSdkVersion: 5.0.1(13)
